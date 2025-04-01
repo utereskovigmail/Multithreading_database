c2cd73f77271c9da54120ded303f1aa960db5441
George <utereskovygmail.com@MacBook-Pro-Yurii.local>
2025-04-01 19:26:39 +0300

    NewHomework

--- FirstSp/DataBaseManager.cs
+++ FirstSp/DataBaseManager.cs
@@ -1,94 +1,124 @@
 using FirstSp.Entities;
 using Bogus;
 namespace FirstSp;
+using Microsoft.EntityFrameworkCore.Storage;
 
 public delegate void DelegateContext(Context context);
 
 
 public class DataBaseManager
+{
+    private Context _threadAppContext;
+
+    public event Action<int> DataInserted;
+    public event Action<Context> GetConnectionEvent;
+
+    //Спеціальний обєкт, який вміє призупитняти довільний потік
+    public static ManualResetEvent mre = new ManualResetEvent(false);
+
+    //public event DeletageContextConnection GetConnectionEvent;
+
+    public DataBaseManager()
     {
-        private Context _context;
-        public event DelegateContext OnContextChanged;
-        private int usersCount = 0; 
-        private object lockObject = new object();
-        public event Action<int> OnUserCountChanged;
+        Thread runConection = new Thread(RunAsyncConnection);
+        runConection.Start();
+    }
 
-        public DataBaseManager()
+    private void RunAsyncConnection()
+    {
+        _threadAppContext = new Context();
+        _threadAppContext.Users.Any();
+        if (GetConnectionEvent != null)
+            GetConnectionEvent(_threadAppContext);
+    }
+
+    public void AddBanansAsync(int count, CancellationToken? token = null)
+    {
+        Thread thread = new Thread(()=>AddBanans(0, count, token));
+        thread.Start();
+    }
+
+    public void AddBanansReallyAsync(int count, CancellationToken? token = null)
+    {
+        int processorCount = Environment.ProcessorCount;
+        int CountPerThread = (count + processorCount - 1) / processorCount;
+        Thread[] threads = new Thread[processorCount];
+        
+        for (int i = 0; i < processorCount; i++)
         {
-            Thread runConnection = new Thread(RunAsyncConnection);
-            runConnection.Start();
-            runConnection.Join();
+            mre.Set();
+            int range = CountPerThread;
+            if (i == processorCount - 1) range = count - i * CountPerThread;
+            
+            Thread thread = new Thread(() => AddBanans(i, range, token));
+            threads[i] = thread;
+            threads[i].Start();
+            
         }
 
-        private void RunAsyncConnection()
+        for (int i = 0; i < threads.Length; i++)
         {
-            _context = new Context();
-            _context.Users.Any();
-            OnContextChanged?.Invoke(_context);
+            if(threads[i].IsAlive) threads[i].Join();
         }
 
-        public void AddUsers(int n)
+        
+    }
+
+    public Faker<User> CreateUserFaker()
+    {
+        string url = "https://picsum.photos/1200/800?grayscale";
+        var faker = new Faker<User>("en")
+            .RuleFor(b => b.Name, f => f.Name.FirstName())
+            .RuleFor(b => b.Surname, f => f.Name.LastName())
+            .RuleFor(b => b.Photo, url);
+        return faker;
+    }
+
+    public void Download(int idx, string Name)
+    {
+        string url = "https://picsum.photos/1200/800?grayscale";
+        string FolderPath = "../../../photos";
+        HttpClient client = new HttpClient();
+        var bytes = client.GetByteArrayAsync(url).Result;
+        string name = $"{Name}{idx}" + ".jpg";
+        var to = Path.Combine(FolderPath, name);
+        File.WriteAllBytes(to, bytes);
+    }
+    
+
+    public void AddBanans(int start, int count, CancellationToken? token = null)
+    {
+        
+        
+        var faker = CreateUserFaker();
+        for (int i = 0; i < count; i++)
         {
-            
-            Thread[] threads = new Thread[n];
-            
-            for (int i = 0; i < n; i++)
+            var b = faker.Generate(1);
+            using (var context = new Context())
             {
-                string name = $"User {i + 1}";
-                string surname = $"Surname {i + 1}";
-                string photoName;
-
-                threads[i] = new Thread(() =>
-                {
-                    using (var context = new Context())
-                    {
-                        photoName = DownloadUserPhoto(name, surname); 
-                        AddAsyncUser(name, surname, photoName, context);
-                        OnContextChanged?.Invoke(context);
-                    }
-
-                    lock (lockObject)
-                    {
-                        usersCount++;
-                        OnUserCountChanged(usersCount);
-                    }
-                    
-                });
-                threads[i].Start();
+                context.Add(b[0]);
+                context.SaveChanges();
+                Download(i+start, b[0].Name);
             }
             
             
-
-            Console.WriteLine($"Added {usersCount} users.");
-        }
-
-        private string DownloadUserPhoto(string name, string surname)
-        {
-            string url = "https://photoboom.ua/uploads/fotoalbom.jpg?1508829419312";
-            HttpClient client = new HttpClient();
             
-            var bytes = client.GetByteArrayAsync(url).Result; 
+            DataInserted?.Invoke(i + 1);
 
-            string folder = "/Users/utereskovygmail.com/RiderProjects/FirstSP/FirstSP/users";
-            string fileName = $"{name}_{surname}.jpg";
-            string filePath = Path.Combine(folder, fileName);
-            
-            File.WriteAllBytes(filePath, bytes);
-    
-            return fileName;
-        }
+            mre.WaitOne(Timeout.Infinite);
 
-
-        private void AddAsyncUser(string Name, string Surname, string photo_name, Context context)
-        {
-            User user = new User
+            if (token != null)
             {
-                Name = Name,
-                Surname = Surname,
-                Photo = photo_name
-            };
+                // Перевіряємо, чи був отриманий сигнал на скасування завдання  
+                if (token.Value.IsCancellationRequested)
+                {
+                    return; // Виходимо з методу, завершуючи виконання завдання  
+                }
+            }
 
-            context.Users.Add(user);
-            context.SaveChanges(); 
         }
+        
     }
+    
+
+}
